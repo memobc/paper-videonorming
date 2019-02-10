5520284ef6a43888d98ccd360731baab97207a07
Rose Cooper <rosecooper0@gmail.com>
2019-02-10 17:17:02 -0500

    Typo fixed

--- Download_Video_clips.R
+++ Download_Video_clips.R
@@ -1,20 +1,17 @@
 #########################################################
-# This scripts downloads videos from the file
-# Video_Summary_Data.csv
+# This script downloads videos from the file
+# Video_Summary_Data
 #########################################################
 
 #Library the R packages
-#Note: you will need to install these package in your version of Rstudio.
+#Note: you will need to install these package in your version of RStudio.
 library(RCurl)
 library(openxlsx)
 
 #Download the videos
 videos <- read.xlsx('Video_Summary_Data.xlsx')
-
-for(i in 104:nrow(videos)){
-  # let user know what video # we are on
+for(i in 1:nrow(videos)){
   cat(paste("Video #",i,"\n"))
-  
   # download the file!
   download.file(videos$URL[i],
                 destfile=paste('./Videos/',videos$Video.Name[i],sep=""))

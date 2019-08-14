74c29402651e4282a7cebcfb095c1d652fdfebe1
Rosalie <rosalie@hagrid.local>
2019-08-14 08:14:59 -0400

    Download script updated for compatibility with PC and Mac

--- Download_Video_clips.R
+++ Download_Video_clips.R
@@ -8,13 +8,42 @@
 library(RCurl)
 library(openxlsx)
 
-#Download the videos
+# Make variable for filesep for compatibility with PC or Mac
+.Platform$file.sep -> filesep
+
+videoPath <- paste0("~", filesep, "Desktop", filesep, "paper-videonorming-master")
+
+# Set working directory as paper-videonorming folder (wherever you have it downloaded)
+setwd(videoPath)
+
+# Determine if running Mac or PC
+substr(.Platform$pkgType, start = 1, stop = 3) -> operatingSystem
+
+# Create videos directory to download videos into
+dir.create(paste0(videoPath, filesep, "Videos"))
+
+# Download the videos
 videos <- read.xlsx('Video_Summary_Data.xlsx')
+
 for(i in 1:nrow(videos)){
+  
+  # Print video info
   cat(paste("Video #",i,"\n"))
-  # download the file!
-  download.file(videos$URL[i],
-                destfile=paste('./Videos/',videos$Video.Name[i],sep=""))
-}
+  
+  # Download the file!
+  
+  # If downloading to PC, use mode="wb" and method="wininet" options
+  if (operatingSystem=="win") {
+    try(
+      download.file(videos$URL[i],
+                    destfile=paste('.', filesep, 'Videos', filesep, videos$Video.Name[i], sep="", method = "wininet", mode="wb")))
+  } # PC loop
+  
+  else {
+    try(
+      download.file(videos$URL[i],
+                    destfile=paste('.', filesep, 'Videos', filesep, videos$Video.Name[i], sep="")))
+  } # Non-PC loop
+} # Video loop
 
 #########################################################
